6869bbe8d91e11fbc004f5d96ea87a26ad89523a
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-09-23 19:57:40 -0500

    add random logo fills

---
+++ src/assets/MansionLogoFills/index.js
@@ -0,0 +1,16 @@
+import art from './art.png';
+import bridge from './bridge.png';
+import clusterfuck from './clusterfuck.png';
+import eyes from './eyes.png';
+import lion from './lion.png';
+import sky from './sky.png';
+import w from './w.png';
+
+export default [art,
+  bridge,
+  clusterfuck,
+  eyes,
+  lion,
+  sky,
+  w,
+];
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -7,29 +7,47 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React, { PropTypes } from 'react';
+import React, { Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import LogoFills from '../../assets/MansionLogoFills';
+import LogoTransparent from '../../assets/mansion_logo_transparent.png';
 import s from './Home.scss';
 import Link from '../../components/Link';
+import { randInt } from '../../core/math';
 
-function Home({ news }) {
-  return (
-    <div className={s.root}>
-      <div className={s.container}>
-        <Link className={s.link} to="/menu">
-          <img className={s.logo} src={require('../../assets/mansion_logo_transparent.png')} />
-        </Link>
+class Home extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      fill: randInt(LogoFills.length)
+    };
+    this.changeFill = this.changeFill.bind(this);
+  }
+
+  changeFill() {
+    let newIdx;
+    do {
+      newIdx = randInt(LogoFills.length);
+    } while (newIdx === this.fill);
+
+    setTimeout(() => {
+      this.setState({ fill: newIdx });
+    }, 100);
+  }
+
+  render() {
+    return (
+      <div className={s.root}>
+        <div className={s.container}>
+          <Link className={s.link} to="/menu" onMouseLeave={this.changeFill}>
+            <img className={s.fill} src={LogoFills[this.state.fill]} />
+            <img className={s.logo} src={LogoTransparent} />
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-Home.propTypes = {
-  news: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
-    contentSnippet: PropTypes.string,
-  })).isRequired,
-};
 
 export default withStyles(Home, s);
